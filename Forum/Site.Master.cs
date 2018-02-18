7afd0a73a76bd0998829b294034a849736997dd4
Gustavo de Abreu Luzia <gusogipa@bol.com.br>
2018-02-18 18:45:18 -0300

    Forum

---
+++ Forum/Site.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Forum
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Verify Cookie
+            HttpCookie cookie = Context.Request.Cookies["ForumXYHelper"];
+
+            if(cookie != null)
+            {
+                Page.ClientScript.RegisterStartupScript(Page.GetType(), "Login", "changeMenusLogin('Welcome " + cookie.Values["NAME"] + "')", true);
+            }
+        }
+    }
+}
